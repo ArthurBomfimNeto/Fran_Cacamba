1f0bff2f0b3779f2fdc85b3751ce4858f18b3375
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-11-14 22:07:14 -0300

    Atualizado a rota de cliente

--- backend/routes/cliente.js
+++ backend/routes/cliente.js
@@ -1,30 +1,45 @@
 const express = require('express')
 const route = express.Router()
 
-route.get('/', (req,res,next) => {
-   res.status(200).send({
+route.get('/', (req, res, next) => {
+    res.status(200).send({
         message: {
-             teste:'deu certo'
-    }
-})
-
-route.post('/', (req, res, next) =>{
-    const cliente = {
-        nome: req.body.nome,
-        cpf : req.body.cpf,
-        dataNascimento : req.body.dataNascimento,
-        email : req.body.email,
-        rua : req.body.rua,
-        bairro :req.body.bairro,
-        numeroCasa : req.body.numeroCasa,
-        cep : req.body.cep
-    }
-
-
-    res.status(201).send({
-       cliente 
+            teste: 'deu certo'
+        }
     })
 })
-   
+route.post('/', (req, res, next) => {
+    const cliente = {
+        nome: req.body.nome,
+        cpf: req.body.cpf,
+        dtNascimento: req.body.dtNascimento,
+        email: req.body.email,
+        rua: req.body.rua,
+        bairro: req.body.bairro,
+        numeroCasa: req.body.numeroCasa,
+        cep: req.body.cep
+    }
+    res.status(201).send({
+        cliente
+    })
 })
+
+route.put('/:teste', (req,res) => {
+    const teste = req.params.teste
+    res.status(200).send({
+        message:{
+            teste: teste
+        }
+    })
+})
+
+route.delete('/',(req, res) => {
+    res.status(200).send({
+        message : {
+            teste : 'Removido'
+        }
+    })
+})
+
+module.exports = route;
 
