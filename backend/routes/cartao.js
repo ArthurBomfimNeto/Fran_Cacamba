6032df4d2616df12cb93f104b786134bb0e848bf
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-11-15 22:42:19 -0300

    Criado a rota cartao

---
+++ backend/routes/cartao.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const route = express.Router()
+
+
+route.get('/', (req, res) => {
+    res.status(200).send({
+        message: {
+            teste: 'Testando GET'
+        }
+    })   
+})
+
+route.post('/', (req, res) => {
+   const cartao = {
+        nome : req.body.nome,
+        numeroCartao : req.body.numeroCartao,
+        cvv : req.body.cvv,
+        mes : req.body.mes,
+        ano : req.body.ano
+    }
+    res.status(201).send({
+        cartao
+    })
+})
+
+route.put('/:teste', (req, res) => {
+    const teste = req.params.teste
+    res.status(200).send({
+        message : {
+            teste : `Testando ${teste}`
+        }
+    })
+})
+
+route.delete('/', (req,res) => {
+    res.status(200).send({
+        message: {
+            teste : 'Teste DELETE'
+        }
+    })
+})
+
+module.exports = route;
+
