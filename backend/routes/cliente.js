954b30cbc8b97078f33acc36f9889d1dff046709
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-11-19 20:38:03 -0300

    Separado por controller

--- backend/routes/cliente.js
+++ backend/routes/cliente.js
@@ -1,45 +1,12 @@
 const express = require('express')
 const route = express.Router()
+const clienteController = require('../controllers/cliente.controller')
 
-route.get('/', (req, res, next) => {
-    res.status(200).send({
-        message: {
-            teste: 'deu certo'
-        }
-    })
-})
-route.post('/', (req, res, next) => {
-    const cliente = {
-        nome: req.body.nome,
-        cpf: req.body.cpf,
-        dtNascimento: req.body.dtNascimento,
-        email: req.body.email,
-        rua: req.body.rua,
-        bairro: req.body.bairro,
-        numeroCasa: req.body.numeroCasa,
-        cep: req.body.cep
-    }
-    res.status(201).send({
-        cliente
-    })
-})
-
-route.put('/:teste', (req,res) => {
-    const teste = req.params.teste
-    res.status(200).send({
-        message:{
-            teste: teste
-        }
-    })
-})
-
-route.delete('/',(req, res) => {
-    res.status(200).send({
-        message : {
-            teste : 'Removido'
-        }
-    })
-})
+route.get('/', clienteController.get)
+route.get('/:id', clienteController.get1)
+route.post('/', clienteController.post)
+route.put('/:id', clienteController.put)
+route.delete('/:id', clienteController.delete)
 
 module.exports = route;
 
