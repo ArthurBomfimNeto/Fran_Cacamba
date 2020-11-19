f4ad0f948bcea71633f356060225152d463d27dc
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-11-18 23:50:26 -0300

    Atualizado o console.log informativo

--- backend/server.js
+++ backend/server.js
@@ -4,5 +4,5 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 server.listen(port, () =>{
-    console.log(`Rodando na porta ${3000}`)
+    console.log(`Ouvindo na porta ${3000}`)
 });
