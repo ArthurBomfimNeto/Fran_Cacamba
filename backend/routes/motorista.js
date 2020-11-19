cfb57fe48748467528f9f4acf4ebbd51d50710fe
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-11-19 20:39:06 -0300

    Separado por controller

--- backend/routes/motorista.js
+++ backend/routes/motorista.js
@@ -1,42 +1,11 @@
 const express = require('express')
 const route = express.Router()
+const motoristaController = require('../controllers/motorista.controller')
 
-route.get('/', (req,res) => {
-    res.status(200).send({
-        message: {
-            teste:'Teste Get'
-        }
-    })
-})
-route.post('/', (req, res) => {
-    res.status(201).send({
-        nome: req.body.nome,
-        cpf: req.body.cpf,
-        cnh : req.body.cnh,
-        dtValidadeCnh : req.body.dtValidadeCnh,
-        dtNascimento: req.body.dtNascimento,
-        email : req.body.email,
-        rua: req.body.rua,
-        bairro: req.body.bairro,
-        numeroCasa: req.body.numeroCasa,
-        cep: req.body.cep
-    })
-})
+route.get('/', motoristaController.get)
+route.get('/:id', motoristaController.get1)
+route.post('/', motoristaController.post)
+route.put('/:id', motoristaController.put)
+route.delete('/:id', motoristaController.delete)
 
-route.put('/:teste', (req, res) => {
-    const teste = req.params.teste
-    res.status(200).send({
-        message : {
-            teste: teste
-        }
-    })
-})
-
-route.delete('/' , (req, res) => {
-    res.status(200).send({
-        message : {
-            teste : 'Teste Delete '
-        }
-    })
-})
 module.exports = route
