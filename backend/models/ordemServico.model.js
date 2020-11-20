de27b6ba9e08a5d54bd4ec5078853018cff8d365
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-11-19 21:24:14 -0300

    Criado o modelo de dados OS

---
+++ backend/models/ordemServico.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+const ordemServico = new Schema({
+    dtLocacao : {type:String, required:true},
+    dtRemocao : {type:String, required:true},
+    rua : {type:String, required:true},
+    bairro : {type:String, required:true},
+    cep : {type:String, required:true},
+    tamanho :{type:String, required:true},
+    residuo : {type:String, required:true},
+    quantidade : {type:String, required:true},
+    valor : {type:String, required:true},
+})
+
+module.exports = mongoose.model('ordemServico', ordemServico)
