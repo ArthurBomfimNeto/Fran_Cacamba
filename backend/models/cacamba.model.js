70f0fa1198bad7e63b7d76940d9d7c4062ab1b1a
Arthur Bomfim Neto <arthur.bomfim.n@gmail.com>
2020-11-20 22:22:39 -0300

    Atualizado o Schema de dados para receber o id de user

--- backend/models/cacamba.model.js
+++ backend/models/cacamba.model.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose')
 
+
 //criar o esquema do banco de dados
 let Schema = mongoose.Schema
 let cacambaSchema = new Schema({
     codCacamba: {type:Number, required: true},
     valor: {type:Number, required: true},
     residuo: {type:String, required:true},
-    tamanho: {type:Number, required: true}
+    tamanho: {type:Number, required: true},
+    user : {type: Schema.Types.ObjectId, ref:'user'}
 })// nomeou a coleção 
 
 //criar o modelo de Dados
